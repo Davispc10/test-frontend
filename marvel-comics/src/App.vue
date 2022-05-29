fb17ac62bdde8cbd9c04cc240dd15dd3cfcec9c7
Felipe Barreto <felipebarreto148@gmail.com>
2022-05-29 02:16:03 -0300

    feat: Created header

--- marvel-comics/src/App.vue
+++ marvel-comics/src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="app"></div>
+	<div id="app">
+		<default-header />
+	</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+	components: { DefaultHeader: () => import("@/components/Header/Header.vue") },
+});
+</script>
 
 <style lang="scss">
 @import "@/scss/_global.scss";
+
+#app {
+  display: flex;
+  flex-direction: column;
+}
 </style>
---
+++ marvel-comics/src/components/Header/Header.vue
@@ -0,0 +1,42 @@
+<template>
+	<header class="header">
+		<img
+			src="../../assets/marvel-logo.png"
+			alt="Logo da marvel, imagem retangular com o fundo vermelho e a palavra marvel escrita em branco."
+			class="header__logo"
+            @click="$route.path != '/' ? $router.push('/') : {}"
+		/>
+	</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+	// setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+	width: 100vw;
+	height: 100px;
+	background: black;
+	box-shadow: 0 0 15px rgba($color: #000000, $alpha: 0.8);
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 5%;
+	transition: 500ms;
+
+	&:hover {
+		box-shadow: 0 0 40px rgba($color: #000000, $alpha: 0.8);
+	}
+
+	&__logo {
+		max-height: 70px;
+        cursor: pointer;
+	}
+}
+</style>
