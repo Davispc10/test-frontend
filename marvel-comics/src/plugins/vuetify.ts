5c499a9939aba12cfd58e6cd84ad5ec6219b04e1
Felipe Barreto <felipebarreto148@gmail.com>
2022-05-31 19:36:55 -0300

    feat: lint

--- marvel-comics/src/plugins/vuetify.ts
+++ marvel-comics/src/plugins/vuetify.ts
@@ -6,21 +6,21 @@ import pt from "vuetify/src/locale/pt";
 Vue.use(Vuetify);
 
 export default new Vuetify({
-	lang: {
-		locales: { pt },
-		current: "pt",
-	},
-	icons: {
-		iconfont: "mdi",
-	},
-	breakpoint: {
-		mobileBreakpoint: "sm",
-		thresholds: {
-			xs: 320,
-			sm: 650,
-			md: 800,
-			lg: 1280,
-		},
-		scrollBarWidth: 24,
-	},
+  lang: {
+    locales: { pt },
+    current: "pt",
+  },
+  icons: {
+    iconfont: "mdi",
+  },
+  breakpoint: {
+    mobileBreakpoint: "sm",
+    thresholds: {
+      xs: 320,
+      sm: 650,
+      md: 800,
+      lg: 1280,
+    },
+    scrollBarWidth: 24,
+  },
 });
--- marvel-comics/src/services/modules/characters.ts
+++ marvel-comics/src/services/modules/characters.ts
@@ -2,24 +2,24 @@ import { api } from "../api";
 import * as Types from "@/types";
 
 export const getCharacters = async (
-	offset: number,
-	limit: number
+  offset: number,
+  limit: number
 ): Promise<Types.CharactersList> => {
-	return await api.get("/characters", { params: { offset, limit } });
+  return await api.get("/characters", { params: { offset, limit } });
 };
 
 export const getCharacterById = async (
-	id: number
+  id: number
 ): Promise<Types.CharactersList> => {
-	return await api.get(`/characters/${id}`);
+  return await api.get(`/characters/${id}`);
 };
 
 export const getCharactersByName = async (
-	nameStartsWith: string
+  nameStartsWith: string
 ): Promise<Types.CharactersList> => {
-	return await api.get("/characters", { params: { nameStartsWith } });
+  return await api.get("/characters", { params: { nameStartsWith } });
 };
 
 export const getComic = async (): Promise<unknown[]> => {
-	return await api.get("/comics/21366");
+  return await api.get("/comics/21366");
 };
