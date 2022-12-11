5df2b8e6a10dac32c508a7585db363f04dc93e11
sydo26 <sydoafk@gmail.com>
2022-12-11 16:32:19 -0300

    fix: marvel endpoint and add marvel private key

--- src/@core/infra/config.ts
+++ src/@core/infra/config.ts
@@ -1,7 +1,9 @@
 const config = {
   marvelPublicApiKey: process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY,
-  marvelEndpoint: "https://developer.marvel.com/v1/public",
-  onlyServer: {},
+  marvelEndpoint: "https://gateway.marvel.com/v1/public",
+  onlyServer: {
+    marvelPrivateKey: process.env.MARVEL_PRIVATE_KEY,
+  },
 };
 
 export default config;
