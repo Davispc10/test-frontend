4fbfa507490b0593e3338922e0fbb1bd8a066cca
sydo26 <sydoafk@gmail.com>
2022-12-11 14:43:53 -0300

    chore: news packages; auto path mapping of tsconfig in jest config; fix tsconfig of cypress

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require("./tsconfig");
+
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   dir: "./",
 });
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   testEnvironment: "jest-environment-jsdom",
 };
 
