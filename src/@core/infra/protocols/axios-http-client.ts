e1b9951c197aca5cac67f19d4902fce1b64c1784
sydo26 <sydoafk@gmail.com>
2022-12-12 11:19:05 -0300

    fix: typescript in Axios Http Client fixed

--- src/@core/infra/protocols/axios-http-client.ts
+++ src/@core/infra/protocols/axios-http-client.ts
@@ -6,15 +6,15 @@ import {
 
 import axios, { AxiosResponse, AxiosInstance } from "axios";
 
-export class AxiosHttpClient implements HttpClient {
+export class AxiosHttpClient<T = any> implements HttpClient<T> {
   constructor(private instance?: AxiosInstance) {
     this.instance = instance ?? axios.create({});
   }
 
-  async request(props: HttpRequest): Promise<HttpResponse> {
+  async request(props: HttpRequest): Promise<HttpResponse<T>> {
     let axiosResponse: AxiosResponse;
     try {
-      axiosResponse = await this.instance.request({
+      axiosResponse = await this.instance.request<{ data: T }>({
         url: props.url,
         method: props.method,
         data: props.body,
