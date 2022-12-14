9f5125595b4d1416c4a6680d2c02730db2d3b439
sydo26 <sydoafk@gmail.com>
2022-12-14 16:54:24 -0300

    chore: add marvel palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        ["marvel-primary"]: "#e23636",
+        ["marvel-black"]: "#202020",
+        ["marvel-white"]: "#FEFEFE",
+        ["marvel-typo"]: "#504a4a",
+        ["marvel-secondary"]: "#f78f3f",
+        ["marvel-accent"]: "#518cca",
+      },
+
       fontFamily: {
         primary: ["var(--font-rubik)", ...fontFamily.sans],
         serif: ["var(--font-rubik)", ...fontFamily.serif],
+        marvel: ["var(--font-marvel)", ...fontFamily.sans],
       },
     },
   },
