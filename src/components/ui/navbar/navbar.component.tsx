dd9d3f6448383ae1044bfa34839558a645ad4825
sydo26 <sydoafk@gmail.com>
2022-12-14 16:55:24 -0300

    feat(app): add navbar in layout

---
+++ src/components/ui/navbar/navbar.component.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { twMerge } from "tailwind-merge";
+
+export type NavbarProps = {} & React.HTMLAttributes<HTMLDivElement>;
+
+export const Navbar: React.FC<NavbarProps> = ({
+  className,
+  children,
+  ...rest
+}) => {
+  const classes = twMerge(
+    "relative flex flex-wrap justify-center items-center w-full p-4 bg-white border-b-2 border-gray-200",
+    className
+  );
+
+  return <nav className={classes}>{children}</nav>;
+};
