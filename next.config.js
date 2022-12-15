2a95509e9f430b60f69192edd6ac1eff9f4883cf
sydo26 <sydoafk@gmail.com>
2022-12-15 17:54:03 -0300

    chore: set redirects

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
     domains: ["i.annihil.us"],
     minimumCacheTTL: 60 * 10, // 10 minutes
   },
+
+  async redirects() {
+    const redirectToCharactersList = ["/", "/characters"];
+    return [
+      ...redirectToCharactersList.map((path) => ({
+        source: path,
+        destination: "/characters/1",
+        permanent: false,
+      })),
+    ];
+  },
 };
 
 module.exports = nextConfig;
