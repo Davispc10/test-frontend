49a587b049cde52761e7822365e9426b03fe8db2
sydo26 <sydoafk@gmail.com>
2022-12-16 10:49:44 -0300

    feat(core/character-http): add findComicsById

--- src/@core/infra/adapters/character-http.adapter.ts
+++ src/@core/infra/adapters/character-http.adapter.ts
@@ -2,13 +2,61 @@ import { Character } from "@core/domain/entities/character";
 import type {
   CharacterAdapter,
   CharacterAdapterFindAllParams,
+  CharacterAdapterFindAllResult,
 } from "@core/domain/adapters/character.adapter";
 import { HttpClient } from "@core/data/protocols/http-client";
+import { Comic } from "@core/domain/entities/comic";
+import {
+  ComicAdapterFindAllParams,
+  ComicAdapterFindAllResult,
+} from "@core/domain/adapters/comic.adapter";
 
 export class CharacterHttpAdapter implements CharacterAdapter {
   constructor(private readonly http: HttpClient) {}
+  async findComicsById(
+    characterId: number,
+    params: ComicAdapterFindAllParams
+  ): Promise<ComicAdapterFindAllResult> {
+    return this.http
+      .request({
+        method: "get",
+        url: `/characters/${characterId}/comics`,
+        params: {
+          limit: String(params.limit),
+          orderBy: String(params.orderBy),
+          offset: String(params.offset),
+        },
+      })
+      .then((res) => {
+        const results = res.data?.data?.results;
+        const available = res.data?.data?.total;
+        const returned = res.data?.data?.count;
 
-  async findAll(params: CharacterAdapterFindAllParams): Promise<Character[]> {
+        if (!Array.isArray(results))
+          throw new Error("Não foi possível accessar as comics!");
+
+        return results
+          ? {
+              comics: results?.map(
+                (comic: any) =>
+                  new Comic({
+                    id: comic.id,
+                    title: comic.title,
+                    description: comic.description,
+                    modified: comic.modified,
+                    thumbnail: comic.thumbnail,
+                  })
+              ),
+              available,
+              returned,
+            }
+          : null;
+      });
+  }
+
+  async findAll(
+    params: CharacterAdapterFindAllParams
+  ): Promise<CharacterAdapterFindAllResult> {
     return this.http
       .request({
         method: "get",
@@ -21,24 +69,28 @@ export class CharacterHttpAdapter implements CharacterAdapter {
       })
       .then((res) => {
         const results = res.data?.data?.results;
+        const available = res.data?.data?.total;
+        const returned = res.data?.data?.count;
 
-        if (!Array.isArray(results)) throw new Error("Results is not a list!");
+        if (!Array.isArray(results))
+          throw new Error("Não foi possível acessar os personagens!");
 
-        return (
-          results?.map(
-            (character: any) =>
-              new Character({
-                id: character.id,
-                name: character.name,
-                description: character.description,
-                modified: character.modified,
-                resourceURI: character.resourceURI,
-                urls: character.urls,
-                thumbnail: character.thumbnail,
-                comics: character.comics.items,
-              })
-          ) ?? null
-        );
+        return results
+          ? {
+              characters: results?.map(
+                (character: any) =>
+                  new Character({
+                    id: character.id,
+                    name: character.name,
+                    description: character.description,
+                    modified: character.modified,
+                    thumbnail: character.thumbnail,
+                  })
+              ),
+              available,
+              returned,
+            }
+          : null;
       });
   }
 
@@ -48,7 +100,8 @@ export class CharacterHttpAdapter implements CharacterAdapter {
       .then((res) => {
         const results = res.data?.data?.results;
 
-        if (!Array.isArray(results)) throw new Error("Results is not a list!");
+        if (!Array.isArray(results))
+          throw new Error("Não foi possível acessar o personagem!");
 
         const result = results[0];
 
@@ -58,10 +111,7 @@ export class CharacterHttpAdapter implements CharacterAdapter {
               name: result.name,
               description: result.description,
               modified: result.modified,
-              resourceURI: result.resourceURI,
-              urls: result.urls,
               thumbnail: result.thumbnail,
-              comics: result.comics.items,
             })
           : null;
       });
