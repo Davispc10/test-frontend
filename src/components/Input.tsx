72654078f58181a256f0036da956eb465a8ced67
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-20 19:32:46 -0300

    feat: implement Input Component

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import { Container, InputText } from "@/styles/components/inputComponent";
+import { useState } from "react";
+
+
+
+export function Input({ ...rest }: React.InputHTMLAttributes<HTMLInputElement>) {
+    const [isFocused, setIsFocused] = useState(false);
+    function handleInputFocuses(){
+        setIsFocused(true);
+    }
+    function handleInputBlur(){
+        setIsFocused(false);
+    }
+    return (
+        <Container focus={isFocused}>
+            🔎
+            <InputText
+                onFocus={handleInputFocuses}
+                onBlur={handleInputBlur}
+                {...rest}
+            />
+        </Container>
+    );
+}
