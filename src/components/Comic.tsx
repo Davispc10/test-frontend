c07c3aafd0f0e564077c7f3f1c0d6b17f92f20ef
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 18:44:59 -0300

    feat: implment and refactoring code of the comics

--- src/components/Comic.tsx
+++ src/components/Comic.tsx
@@ -2,20 +2,23 @@ import { memo } from "react";
 
 import Image from "next/image";
 import { Container, ComicName } from "@/styles/components/comicComponent";
+import { Thumbnail } from "../@core/domain/entities/Hero";
 
 
 interface props {
     title: string;
+    thumbnail: Thumbnail;
+    thumbnailFormatted: (T: Thumbnail) => string;
 }
-function Comic({ title }: props) {
+function Comic({ title, thumbnail, thumbnailFormatted }: props) {
     
     return (
         <Container>
             <Image
                 height={200}
                 width={150}
-                src={'/quadrinho.jpg'}
-                alt={title}
+                src={thumbnailFormatted(thumbnail)}
+                alt={thumbnailFormatted(thumbnail)}
             />
             <ComicName>{title.length > 30 ? `${title.slice(0, 30)}...`: title}</ComicName>
         </Container>
