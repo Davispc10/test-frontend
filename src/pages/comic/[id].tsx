807eac54faaa6e73a93a6d9e2ef16637f26858cd
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 14:25:48 -0300

    feat: refactoring comic/[:id]

--- src/pages/comic/[id].tsx
+++ src/pages/comic/[id].tsx
@@ -7,13 +7,19 @@ import CardCharacterComic from "@/src/components/CardCharacterComic";
 export default function PageComic() {
     const { query, push } = useRouter();
 
+    //query.id no array de dependência para o caso de quando nesse componente com um id diferente
+    //a requisição será refeita
     const { isLoading, error, data } = useQuery(['query', query.id], async () => {
         return FactoryMakeListByIdHeroUseCase().execute(Number(query.id));
     });
     
+    //tiver erro na requisição volta para a tela de heros
     if(error) {push('/')}
     return (
         <>
+            {/* Sempre é bom pensar na experiência de usuário
+                e o useQuery me ajuda nessa tarefa
+            */}
             {isLoading ? <ContainerLoading>Loading...</ContainerLoading>
                 :
                 <Container>
