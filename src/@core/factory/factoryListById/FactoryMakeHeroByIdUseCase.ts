347dc280f9d4bb7e8d101ce50207b0c26dfef5ec
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 14:28:12 -0300

    feat: implement comments FactoryMakeListHeroById

--- src/@core/factory/factoryListById/FactoryMakeHeroByIdUseCase.ts
+++ src/@core/factory/factoryListById/FactoryMakeHeroByIdUseCase.ts
@@ -2,6 +2,9 @@ import { ListHeroByIdUseCase } from "../../application/Hero/listHeroById.useCase
 import { AxiosHttpGateway } from "../../infra/gateways/axios-http.geteway";
 import { HeroHttpGateway } from "../../infra/gateways/herosGatewayHttp";
 
+//Factory é apenas a fabricação dos meus requests
+//Onde optei por criar os requests de forma separadamente
+//no caso de eu querer mudar algo em uma request, mudar apenas nela e não em todas
 export function FactoryMakeListByIdHeroUseCase() {
     const httpGateway = new AxiosHttpGateway();
     const heroGateway = new HeroHttpGateway(httpGateway, String(process.env.NEXT_PUBLIC_BASEURL), String(process.env.NEXT_PUBLIC_PUBLICKEY), 1, 'name', 0);
