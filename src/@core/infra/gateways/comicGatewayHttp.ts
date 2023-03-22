81135e865ad5b8271c9d99fa5f8455da6cfa811e
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 18:46:56 -0300

    feat: implement gateway of the comicGateway for separating useCase of hero and comic

---
+++ src/@core/infra/gateways/comicGatewayHttp.ts
@@ -0,0 +1,22 @@
+import { OrderBy } from "../../application/Hero/listHeros.useCase";
+import { Comic } from "../../domain/entities/Comic";
+import { Hero } from "../../domain/entities/Hero";
+import { ComicGateway } from "../../domain/gateways/comic.gateway";
+import { HttpGateway } from "../../gateway/http";
+import { HttpResponse } from "../../protocols/httpClient";
+
+//Criação do gateway de comic, como Comic seria outra regra de outro caso de uso
+//É importando isola-lo para evitar divergências com o Hero
+export class ComicHttpGateway implements ComicGateway{
+    constructor(private http: HttpGateway, private endpoint: string, private apiKey: string) {}
+    async findComics(id: Number): Promise<Comic[]> {
+        const response: HttpResponse = await this.http.request({
+            method: 'get',
+            url: `${this.endpoint}/characters/${id}/comics`,
+            params: { 
+               apikey: this.apiKey,
+           }});
+        return response.data.data.results;
+    }
+
+}
