51a9f9edf79a7722ef20a22692dfaf214941cad1
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 14:29:02 -0300

    feat: refactoring factoryListComics

--- src/@core/factory/factoryListComics/FactoryMakeListComicsUseCase.ts
+++ src/@core/factory/factoryListComics/FactoryMakeListComicsUseCase.ts
@@ -1,11 +1,14 @@
-import { ListHeroUseCase } from "../../application/Comic/listComics.useCase";
+import { ListComicsUseCase } from "../../application/Comic/listComics.useCase";
 import { AxiosHttpGateway } from "../../infra/gateways/axios-http.geteway";
 import { HeroHttpGateway } from "../../infra/gateways/herosGatewayHttp";
 
+//Factory é apenas a fabricação dos meus requests
+//Onde optei por criar os requests de forma separadamente
+//no caso de eu querer mudar algo em uma request, mudar apenas nela e não em todas
 export function FactoryMakeComicsUseCase(offset = 0) {
     const httpGateway = new AxiosHttpGateway();
     const heroGateway = new HeroHttpGateway(httpGateway, String(process.env.NEXT_PUBLIC_BASEURL), String(process.env.NEXT_PUBLIC_PUBLICKEY), 12, 'name', offset);
-    const useCase = new ListHeroUseCase(heroGateway);
+    const useCase = new ListComicsUseCase(heroGateway);
 
     return useCase;
 }
