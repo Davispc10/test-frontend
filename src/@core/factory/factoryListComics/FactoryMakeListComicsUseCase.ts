7c9967dcf5f36f731ea2c79021439d048b25a1b1
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 18:43:31 -0300

    feat: refactoring useCase and adapting for the request

--- src/@core/factory/factoryListComics/FactoryMakeListComicsUseCase.ts
+++ src/@core/factory/factoryListComics/FactoryMakeListComicsUseCase.ts
@@ -1,13 +1,13 @@
 import { ListComicsUseCase } from "../../application/Comic/listComics.useCase";
 import { AxiosHttpGateway } from "../../infra/gateways/axios-http.geteway";
-import { HeroHttpGateway } from "../../infra/gateways/herosGatewayHttp";
+import { ComicHttpGateway } from "../../infra/gateways/comicGatewayHttp";
 
 //Factory é apenas a fabricação dos meus requests
 //Onde optei por criar os requests de forma separadamente
 //no caso de eu querer mudar algo em uma request, mudar apenas nela e não em todas
-export function FactoryMakeComicsUseCase(offset = 0) {
+export function FactoryMakeComicsUseCase() {
     const httpGateway = new AxiosHttpGateway();
-    const heroGateway = new HeroHttpGateway(httpGateway, String(process.env.NEXT_PUBLIC_BASEURL), String(process.env.NEXT_PUBLIC_PUBLICKEY), 12, 'name', offset);
+    const heroGateway = new ComicHttpGateway(httpGateway, String(process.env.NEXT_PUBLIC_BASEURL), String(process.env.NEXT_PUBLIC_PUBLICKEY));
     const useCase = new ListComicsUseCase(heroGateway);
 
     return useCase;
