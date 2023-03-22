6e8dcc2475a3cec71ccc712333605f1563993b18
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-22 14:27:32 -0300

    feat: refactoring FactoryMakeListHero

--- src/@core/factory/factoryListAll/FactoryMakeListHeroUseCase.ts
+++ src/@core/factory/factoryListAll/FactoryMakeListHeroUseCase.ts
@@ -2,6 +2,9 @@ import { ListHeroUseCase } from "../../application/Hero/listHeros.useCase";
 import { AxiosHttpGateway } from "../../infra/gateways/axios-http.geteway";
 import { HeroHttpGateway } from "../../infra/gateways/herosGatewayHttp";
 
+//Factory é apenas a fabricação dos meus requests
+//Onde optei por criar os requests de forma separadamente
+//no caso de eu querer mudar algo em uma request, mudar apenas nela e não em todas
 export function FactoryMakeListUseCase(offset = 0) {
     const httpGateway = new AxiosHttpGateway();
     const heroGateway = new HeroHttpGateway(httpGateway, String(process.env.NEXT_PUBLIC_BASEURL), String(process.env.NEXT_PUBLIC_PUBLICKEY), 12, 'name', offset);
