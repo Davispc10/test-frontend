0165ea16efcac2cd88228cef37b30d53c48a2a65
Bruno Sales <bruno.particular25@hotmail.com>
2023-03-23 13:39:29 -0300

    feat: implement hook into index and refactoring code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,18 +14,17 @@ import {
   HomeContainer, 
   PaginationContainer 
 } from '@/styles/pages/home'
+import { useRequest } from '../hooks/requests';
 
 function Home() {
-  const [offset, setOffSet] = useState(0);
+  // const [offset, setOffSet] = useState(0);
   const [stringPattern, setStringPattern] = useState('');
 
-  //offset como array de dependência para quando o estado mudar a função rodar novamente
-  const { isLoading, error, data } = useQuery(['query', offset], async () => {
-    return FactoryMakeListUseCase(offset).execute();
-  });
+  //usando meu contexto e hook criado por mim
+  const { isLoading, data, offset, handleSetOffset } = useRequest();
 
   //DataAndSearch vai ser onde vou armazenar o meu data de Heroes
-  //E caso digita algo ele pesquisa no array o heroi com aquele nome
+  //E caso alguem digite ele pesquisa no array o heroi com aquele nome
   const dataAndSearch: Hero[] = useMemo(() => {
     if (stringPattern) {
       const search = data?.filter((hero) =>
@@ -41,12 +40,16 @@ function Home() {
       {isLoading ? <ContainerLoading>Loading...</ContainerLoading>
       : <HomeContainer>
         <PaginationContainer>
-          <Button focus={offset === 0} onClick={() => setOffSet(0)}>1</Button>
-          <Button focus={offset === 13} onClick={() => setOffSet(13)}>2</Button>
-          <Button focus={offset === 23} onClick={() => setOffSet(23)}>3</Button>
-          <Button focus={offset === 33} onClick={() => setOffSet(33)}>4</Button>
-          <Button focus={offset === 43} onClick={() => setOffSet(43)}>5</Button>
-          <Button focus={offset === 53} onClick={() => setOffSet(53)}>6</Button>
+          {/* Paginação simples e prática usando o offset para a API 
+              Em uma refatoração pode-se fazer um calculo para obter o numero maximo de botões 
+              com base no número de herois
+          */}
+          <Button focus={offset === 0} onClick={() => handleSetOffset(0)}>1</Button>
+          <Button focus={offset === 13} onClick={() => handleSetOffset(13)}>2</Button>
+          <Button focus={offset === 23} onClick={() => handleSetOffset(23)}>3</Button>
+          <Button focus={offset === 33} onClick={() => handleSetOffset(33)}>4</Button>
+          <Button focus={offset === 43} onClick={() => handleSetOffset(43)}>5</Button>
+          <Button focus={offset === 53} onClick={() => handleSetOffset(53)}>6</Button>
         </PaginationContainer>
         <Input onChange={(event) => setStringPattern(event.target.value)}/>
         <CardsContainer>
