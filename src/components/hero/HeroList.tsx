b7bef3196d8030ff76c9827fde4506091820fe70
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-03-25 21:30:42 -0300

    fix: fixing responsivity of hero list component

--- src/components/hero/HeroList.tsx
+++ src/components/hero/HeroList.tsx
@@ -4,7 +4,7 @@ import HeroCard from "./HeroCard";
 
 const HeroList = ({ heroes }: { heroes: Hero[] }) => {
   return (
-    <main className="grid min-h-screen grid-cols-5 gap-8 px-20 mt-8 place-content-start">
+    <main className="grid grid-cols-1 gap-8 px-10 py-8 lg:px-20 lg:grid-cols-5 place-content-start">
       {/* Hero list (hero card) */}
       {heroes.map((hero, i) => (
         <HeroCard hero={hero} key={i} />
