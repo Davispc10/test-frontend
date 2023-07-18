d2a7d20238926995c91b950285cb7ac03279e239
Gabriel <gabrigomez15@gmail.com>
2023-07-18 10:26:35 -0300

    feat: add fonts and navBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { NavBar } from './components/NavBar'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='font-bangers'>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
