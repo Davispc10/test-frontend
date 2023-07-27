8cf485265cd56c47f373447aa8660c5790d293dd
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-26 22:37:36 -0300

    feat: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header>
+      <div>
+        <img />
+      </div>
+
+      <div>
+        <input placeholder="Pesquisar heroi" type="text" name="" id="" />
+      </div>
+
+      <div />
+    </header>
+  );
+};
+
+export default Header;
