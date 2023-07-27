eccd2b228912696a9413b1867f1991a53e34c198
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-26 22:02:55 -0300

    feat: add react query and hooks grouper

--- src/components/ApplicationContainer/index.tsx
+++ src/components/ApplicationContainer/index.tsx
@@ -1,46 +1,29 @@
 "use client";
 import { NextPage } from "next";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { Provider } from "react-redux";
-import store from "@/redux/store";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-// import styles from "./styles.module.css";
-// import HooksGrouper from "@/src/hooks/HooksGrouper";
-// import ToastContainer from "../ToastContainer";
-// import Loading from "../Loading";
-// import { PrivateRoute } from "../PrivateRoute";
-// import { usePathname } from "next/navigation";
-// import { checkIsPublicRoute } from "@/src/functions/check-is-public-route";
+import store from "@/redux/store";
+import ToastContainer from "../ToastContainer";
+import Loading from "../Loading";
+import HooksGrouper from "@/hooks";
+
+const queryClient = new QueryClient();
 
 interface Props {
   children: ReactNode;
 }
 
 const ApplicationContainer: NextPage<Props> = ({ children }) => {
-  // const pathname = usePathname();
-
-  // const [isPublicPage, setIsPublicPage] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   setIsPublicPage(checkIsPublicRoute(pathname));
-  // }, [pathname]);
-
   return (
-    <Provider store={store}>
-      {/* <HooksGrouper>
-      <ToastContainer />
-      <Loading />
-      {isPublicPage && children}
-      {!isPublicPage && (
-        <PrivateRoute>
-      <div className={`w-screen h-screen ${styles.container}`}>
-        <LateralMenu /> */}
-      {children}
-      {/* </div>
-        </PrivateRoute>
-      )}
-    </HooksGrouper> */}
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ToastContainer />
+        <Loading />
+        <HooksGrouper>{children}</HooksGrouper>
+      </Provider>
+    </QueryClientProvider>
   );
 };
 
