d9de92d305ad36e4d8926808834f55f619959ddd
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-26 22:01:11 -0300

    feat: toastify component

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,8 @@
+import { ToastContainer as ReactToastifyContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastContainer = () => {
+  return <ReactToastifyContainer theme="colored" />;
+};
+
+export default ToastContainer;
