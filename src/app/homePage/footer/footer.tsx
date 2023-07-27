4b5909ca0e6ef0d1f286003f23a8a0676fb6bf2b
Rafael Andrade <ra-fasilva2010@hotmail.com>
2023-07-27 13:56:50 -0300

    fix: high in footer

--- src/app/homePage/footer/footer.tsx
+++ src/app/homePage/footer/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <div className="fixed bg-gray-700 h-14 bottom-0 w-full">
+      <div className="fixed bg-gray-700 h-12 bottom-0 w-full">
   
       </div>
     )
