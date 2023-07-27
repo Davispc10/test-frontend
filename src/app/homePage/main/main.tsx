f1c04de4a1c1f38d99a44609b3a5fae148dc21f1
Rafael Andrade <ra-fasilva2010@hotmail.com>
2023-07-27 13:59:08 -0300

    feat: update cards and add a carousel

--- src/app/homePage/main/main.tsx
+++ src/app/homePage/main/main.tsx
@@ -1,55 +1,78 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import React  from 'react';
 import axios from 'axios';
 import md5 from 'md5';
 import Card from './cards/cards';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const publicKey = 'fbc4f64b3d3b579945901a02361c004e';
 const privateKey = '56510ad02017aee2e74689a77269878198c8b40f';
 
 export default function Main() {
-  const [url, setUrl] = useState('https://gateway.marvel.com:443/v1/public/characters?limit=6&offset=0');
+  const [url] = useState('https://gateway.marvel.com:443/v1/public/characters');
   const [herois, setHerois] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
+  const carousel = useRef(null);
   const timestamp = new Date().getTime();
   const hash = md5(timestamp + privateKey + publicKey);
 
-  const getAllPokemon = async (url: string) => {
-    localStorage.removeItem('pesquisou');
-    localStorage.setItem('pesquisou', 'true');
-    const result = await axios.get(url, {
+  const getAllCharacter = async (url: string) => {
+    await axios.get(url, {
       params: {
         ts: timestamp,
         apikey: publicKey,
         hash: hash,
       }
-    })
-      .then(result => {
+    }).then(result => {
           setHerois(result.data.data.results);
           setLoading(false);
+          console.log(result);
         }
-      )
-      .catch(e => setLoading(true));
-    console.log(herois);
-    // console.log(result.data.data);
-    // setHerois(result.data.results);
+      ).catch(() => setLoading(true));
+  }
 
-    // const getPokemon = async (res) => {
-    //     const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${res}`);
-    //     setHerois(state => {
-    //         state = [...state, result.data];
-    //         return state;
-    //     });
-    // }
+  const handleSearch = () => {
+    setIsSearching(!isSearching);
+  };
 
-    //result.data.results.map((herois) => getPokemon(herois.name));
-}
+  const handleScrollWithAnimation = (scrollValue, duration) => {
+    const element = carousel.current;
+    const startTime = performance.now();
+    const startScrollLeft = element.scrollLeft;
 
-useEffect(() => {
-  getAllPokemon(url);
-}, [url]);
+    const animateScroll = (timestamp) => {
+      const elapsed = timestamp - startTime;
+      const progress = Math.min(elapsed / duration, 1);
+      const newScrollLeft = startScrollLeft + (scrollValue - startScrollLeft) * progress;
+      element.scrollLeft = newScrollLeft;
+
+      if (progress < 1) {
+        requestAnimationFrame(animateScroll);
+      }
+    };
+
+    requestAnimationFrame(animateScroll);
+  };
+
+  const buttonRight = (e) => {
+    e.preventDefault();
+    const scrollValue = carousel.current.scrollLeft - carousel.current.offsetWidth;
+    handleScrollWithAnimation(scrollValue, 300);
+  }
+
+  const buttonLeft = (e) => {
+    e.preventDefault();
+    const scrollValue = carousel.current.scrollLeft + carousel.current.offsetWidth;
+    handleScrollWithAnimation(scrollValue, 300);
+  }
+
+  useEffect(() => {
+    getAllCharacter(url);
+  }, [url]);
 
   return (
     loading ? (
@@ -57,8 +80,8 @@ useEffect(() => {
         <p>Loading</p>
       </div>
     ) : (
-      <div>
-        <div className="grid grid-rows-3 grid-cols-2">
+      <div className="w-screen overflow-y-hidden">
+        <div className="grid grid-rows-3 grid-flow-col overflow-x-auto mx-1 overflow-hidden delay-150" ref={carousel}>
           {
             herois && herois.sort((idA, idB) =>
                 idA.id - idB.id
@@ -69,15 +92,40 @@ useEffect(() => {
                 />
             )
           }
-            {/* <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card /> */}
         </div>
-        <div>
-
+        <div className="flex grid-cols-3 w-full items-center justify-center">
+          <button className="h-9 w-9 bg-red-600 rounded-full cursor-none"
+            onClick={buttonRight}>
+            <FontAwesomeIcon icon={faArrowLeft}  className="text-white text-xl"/>
+          </button>
+          {/* {isSearching ? (
+            <input
+              type="text"
+              className={`px-4 py-2 rounded-md border border-gray-300`}
+              placeholder="Search..."
+              onBlur={handleSearch}
+            />
+          ) : (
+            <button
+              onClick={handleSearch}
+              className={`h-9 w-9 bg-red-600 rounded-full cursor-none transform ${
+                isSearching ? 'scale-0 opacity-0 pointer-events-none' : 'scale-100 opacity-100 pointer-events-auto'
+              } transition-all duration-300 ease-in-out`}
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white text-xl"/>
+            </button>
+          )} */}
+          <div className="search-box">
+            <button className="btn-search">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white text-xl"/>
+            </button>
+            <input
+              className="input-search"
+            />
+          </div>
+          <button className="h-9 w-9 bg-red-600 rounded-full cursor-none" onClick={buttonLeft}>
+            <FontAwesomeIcon icon={faArrowRight} className="text-white text-xl"/>
+          </button>
         </div>
       </div>
     )
