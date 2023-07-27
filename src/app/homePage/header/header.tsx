1c08544a5054f7c6d67247e690cce32e1204bafe
Rafael Andrade <ra-fasilva2010@hotmail.com>
2023-07-27 13:57:11 -0300

    feat: add the logo in header

--- src/app/homePage/header/header.tsx
+++ src/app/homePage/header/header.tsx
@@ -1,7 +1,15 @@
 export default function Header() {
-  return (
-    <div className="fixed bg-gray-700 h-14 top-0 w-full">
+  const logo = `/Marvel-Logo.png`;
 
+  return (
+    <div className="fixed bg-gray-700 h-12 top-0 w-full">
+      <div className="grid w-full h-full items-center justify-items-center justify-content-center">
+        <img
+            src= {logo}
+            alt=''
+            className='relative object-cover w-24 h-full rounded-t-lg top-6'
+        />
+      </div>
     </div>
   )
 }
