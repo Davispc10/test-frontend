b846500eb79c05885ec4c02fa6eda3417f207df1
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-07-28 14:23:07 -0300

    change layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react"
 import { usePagination, Pagination } from 'pagination-react-js'
+
+import LayoutDefault from '@/components/layouts/default'
 import Character from "@/components/character"
 import { useGetCharactersQuery } from '../store/services/marvel'
 import { params } from '@/helpers'
@@ -29,58 +31,56 @@ export default function Home() {
     }
   }, [isLoading, dispatch, data])
 
-  useEffect(() => {
-    console.log('characters', characters)
-  }, [characters])
-
   return (
-    <main className={`${isLoading ? 'showLoader' : ''}`}>
-      {error ? (
-        <div className="">Oh no, there was an error</div>
-      ) : isLoading ? (
-        <div className="loader" />
-      ) : data ? (
-        <div className="container">
-          {characters?.results?.slice(entries.indexOfFirst, entries.indexOfLast)
-            .map(({ id, name, thumbnail, ...props }: dataMarvelProps) => (
-              <Character
-                key={id}
-                name={name}
-                thumbnail={`${thumbnail.path}.${thumbnail.extension}`}
-                {...props}
-              />
-          ))}
-        </div>
-      ) : null
-    }
+    <LayoutDefault>
+      <main className={`layoutDefault ${isLoading ? 'showLoader' : ''}`}>
+        {error ? (
+          <div className="">Oh no, there was an error</div>
+        ) : isLoading ? (
+          <div className="loader" />
+        ) : data ? (
+          <div className="container">
+            {characters?.results?.slice(entries.indexOfFirst, entries.indexOfLast)
+              .map(({ id, name, thumbnail, ...props }: dataMarvelProps) => (
+                <Character
+                  key={id}
+                  name={name}
+                  thumbnail={`${thumbnail.path}.${thumbnail.extension}`}
+                  {...props}
+                />
+            ))}
 
-      <div className="pagination-container">
-        <Pagination
-          entriesPerPage={entriesPerPage.get}
-          totalEntries={100}
-          currentPage={{ get: currentPage.get, set: currentPage.set }}
-          offset={3}
-          classNames={{
-            wrapper: "pagination m-auto",
-            item: "pagination-item",
-            itemActive: "pagination-item-active",
-            navPrev: "pagination-item nav-item nav-prev",
-            navNext: "pagination-item nav-item nav-next",
-            navStart: "pagination-item nav-item",
-            navEnd: "pagination-item nav-item",
-            navPrevCustom: "pagination-item",
-            navNextCustom: "pagination-item"
-          }}
-          showFirstNumberAlways={true}
-          showLastNumberAlways={true}
-          navStart="&#171;"
-          navEnd="&#187;"
-          navPrev="&#x2039;"
-          navNext="&#x203a;"
-          navPrevCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" }}
-          navNextCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" }}
-        />
-      </div>
-    </main>
+            <div className="pagination-container">
+              <Pagination
+                entriesPerPage={entriesPerPage.get}
+                totalEntries={100}
+                currentPage={{ get: currentPage.get, set: currentPage.set }}
+                offset={3}
+                classNames={{
+                  wrapper: "pagination m-auto",
+                  item: "pagination-item",
+                  itemActive: "pagination-item-active",
+                  navPrev: "pagination-item nav-item nav-prev",
+                  navNext: "pagination-item nav-item nav-next",
+                  navStart: "pagination-item nav-item",
+                  navEnd: "pagination-item nav-item",
+                  navPrevCustom: "pagination-item",
+                  navNextCustom: "pagination-item"
+                }}
+                showFirstNumberAlways={true}
+                showLastNumberAlways={true}
+                navStart="&#171;"
+                navEnd="&#187;"
+                navPrev="&#x2039;"
+                navNext="&#x203a;"
+                navPrevCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" }}
+                navNextCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" }}
+              />
+            </div>
+          </div>
+        ) : null
+      }
+      </main>
+    </LayoutDefault>
   )
 }
