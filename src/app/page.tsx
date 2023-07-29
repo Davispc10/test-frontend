feb94b1de69017c2402e6307bba8fa0f28b2198b
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-29 18:33:24 -0300

    feat: render body component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Header from "@/components/Header";
+import Body from "@/components/Body";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Body />
     </main>
   );
 }
