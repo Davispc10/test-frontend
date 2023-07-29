978c3fcb173ec49603d4e5fa6509cf4cc7d3e888
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-29 18:30:55 -0300

    feat: pass tokens to api request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,10 @@
 import axios from "axios";
+import md5 from "md5";
+
+const TS = new Date().getTime();
+const PRIVATE_KEY = process.env.NEXT_PUBLIC_API_PRIVATE_TOKEN;
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_API_PUBLIC_TOKEN;
+const hash = md5(`${TS.toString()}${PRIVATE_KEY}${PUBLIC_KEY}`);
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -7,6 +13,8 @@ export const api = axios.create({
     Accept: "application/json",
   },
   params: {
-    apikey: process.env.NEXT_PUBLIC_API_PUBLIC_TOKEN,
+    ts: TS,
+    hash,
+    apikey: PUBLIC_KEY,
   },
 });
