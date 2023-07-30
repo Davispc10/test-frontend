89fbd25541a12fe9e5d2223131fd6e1ab453a117
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-30 11:12:13 -0300

    feat: paginate application

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,12 +1,26 @@
-import { Hero } from "@/types/heroes";
+import "@/app/globals.css";
+import { Hero, HeroesListReducer } from "@/types/heroes";
 import React, { useCallback } from "react";
 import HeroCard from "../HeroCard";
+
+import Pagination from "../Pagination";
+import { useDispatch } from "react-redux";
+import {
+  changeHeroesPage,
+  updateItemsOffset,
+} from "@/redux/heroesList/actions";
+import { useSelector } from "react-redux";
 
 type Props = {
   heroesList: Hero[];
 };
 
 const HeroesList = ({ heroesList }: Props) => {
+  const { perPage } = useSelector(
+    (state: HeroesListReducer) => state.heroesListReducer
+  );
+  const dispatch = useDispatch();
+
   const renderHeroesList = useCallback(
     (data: Hero[]) => {
       return (
@@ -20,10 +34,21 @@ const HeroesList = ({ heroesList }: Props) => {
     [heroesList]
   );
 
+  const handlePageClick = (event: { selected: number }): void => {
+    dispatch(changeHeroesPage(event.selected));
+
+    const newOffset = (event.selected * perPage) % heroesList.length;
+    dispatch(updateItemsOffset(newOffset));
+  };
+
   return (
-    <div>
+    <div className="flex justify-between items-center flex-wrap p-8 gap-4">
       {heroesList && heroesList.length > 0 ? (
-        renderHeroesList(heroesList)
+        <>
+          {renderHeroesList(heroesList)}
+
+          <Pagination onChange={handlePageClick} />
+        </>
       ) : (
         <p>Não há herois a serem listados</p>
       )}
