986b3860c1dabd2fe608be9831df95689d9229e0
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-30 11:06:48 -0300

    feat: allow marvel domain to request images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["i.annihil.us"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
