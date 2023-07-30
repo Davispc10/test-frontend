d05b1a8fa143d0ed146b68a23d741fbbbfafb3da
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-07-30 11:10:18 -0300

    feat: pagination component

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,40 @@
+import { HeroesListReducer } from "@/types/heroes";
+import React from "react";
+import ReactPaginate from "react-paginate";
+import { useSelector } from "react-redux";
+type Props = {
+  onChange: (event: { selected: number }) => void;
+};
+
+const Pagination = ({ onChange }: Props) => {
+  const { totalPages } = useSelector(
+    (state: HeroesListReducer) => state.heroesListReducer
+  );
+
+  return (
+    <div className="w-full mt-8 flex items-center justify-center">
+      <ReactPaginate
+        pageCount={totalPages}
+        pageRangeDisplayed={5}
+        renderOnZeroPageCount={null}
+        breakLabel="..."
+        nextLabel="próximo >"
+        previousLabel="< anterior"
+        onPageChange={onChange}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        disabledClassName="pagination-disabled"
+      />
+    </div>
+  );
+};
+
+export default Pagination;
