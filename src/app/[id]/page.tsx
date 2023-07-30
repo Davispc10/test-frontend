0b0988183564d433c00ac5682357a7e11d58be4b
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-30 16:43:21 -0300

    feat: add description validate and improving styles

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import { CharacterInfo } from './components/CharacterInfo'
 import { CharacterInfoPropsSchema } from './components/CharacterInfo.schema'
 import { data } from 'autoprefixer'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 
 export interface HeroPropsSchema {
   params: {
@@ -16,6 +18,8 @@ export default async function Hero({ params }: HeroPropsSchema) {
     const getCharacterResponse = await getCharacter({ id: params.id })
 
     const character = getCharacterResponse.data.results[0]
+
+    const description = character.description || 'descrição não informada'
 
     const characterInfos: CharacterInfoPropsSchema[] = [
       {
@@ -37,26 +41,34 @@ export default async function Hero({ params }: HeroPropsSchema) {
     const characterThumbnail = getCharacterImagePath({ thumbnailPath })
 
     return (
-      <div className="flex justify-between text-white">
-        <Image
-          height={800}
-          width={400}
-          src={characterThumbnail}
-          alt={character.name}
-          className="h-96 w-72  bg-gray-950 object-cover "
-        />
-        <div className="flex max-w-3xl flex-col gap-3">
-          <h1 className="text-3xl font-bold">{character.name}</h1>
-          <p>{character.description}</p>
+      <div>
+        <Link
+          href={'/'}
+          className="mb-5 flex w-max items-center gap-2 rounded-lg bg-red-700 px-2 py-1 text-white"
+        >
+          <ArrowLeft size={18} /> Voltar
+        </Link>
+        <div className="flex justify-around text-white">
+          <Image
+            height={800}
+            width={400}
+            src={characterThumbnail}
+            alt={character.name}
+            className="sticky top-14 h-96 w-72 bg-gray-950 object-cover"
+          />
+          <div className="flex max-w-3xl flex-col gap-3">
+            <h1 className="text-3xl font-bold">{character.name}</h1>
+            <p>{description}</p>
 
-          <div className="flex flex-col gap-3">
-            {characterInfos.map((characterInfo) => (
-              <CharacterInfo
-                key={characterInfo.title}
-                items={characterInfo.items}
-                title={characterInfo.title}
-              />
-            ))}
+            <div className="flex flex-col gap-3">
+              {characterInfos.map((characterInfo) => (
+                <CharacterInfo
+                  key={characterInfo.title}
+                  items={characterInfo.items}
+                  title={characterInfo.title}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
