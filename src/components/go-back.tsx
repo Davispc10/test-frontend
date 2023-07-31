c056e781d6a5ca04fee35f209514ba09aa53e5f0
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-07-30 21:32:26 -0300

    add loading when clicking on go back button

--- src/components/go-back.tsx
+++ src/components/go-back.tsx
@@ -1,27 +1,43 @@
 'use client'
 
+import { useTransition } from 'react'
+
 import { useRouter } from 'next/navigation'
 
 import { Icons } from '@/components/icons'
 import { Button } from '@/components/ui/button'
+
+import { Spinner } from './ui/spinner'
 
 type GoBackProps = {
   className?: string
 }
 
 export const GoBack = ({ className }: GoBackProps) => {
+  const [isPending, startTransition] = useTransition()
   const router = useRouter()
 
   const hasPreviousPage =
     typeof window !== 'undefined' && window.history.length > 1
 
+  const handleOnClick = () => {
+    startTransition(() => {
+      hasPreviousPage ? router.back() : router.push('/')
+    })
+  }
+
   return (
     <Button
       variant="ghost"
-      onClick={hasPreviousPage ? router.back : () => router.push('/')}
+      onClick={handleOnClick}
       className={className}
+      disabled={isPending}
     >
-      <Icons.ArrowLeft className="mr-2 h-4 w-4" />
+      {isPending ? (
+        <Spinner className="mr-2 h-4 w-4" />
+      ) : (
+        <Icons.ArrowLeft className="mr-2 h-4 w-4" />
+      )}
       Go Back
     </Button>
   )
