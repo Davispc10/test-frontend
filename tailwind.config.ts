b2b66405eba2696c189c8eea2c00293b296cab94
João Feitoza <joaofeitoza.13@gmail.com>
2023-10-07 14:26:07 -0300

    chore: add tailwind and global css initial configs

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-100": "#F6F9E8",
+        "primary-200": "#ADADAA",
+        "primary-400": "#999999",
+        "primary-500": "#555555",
+        "primary-700": "#2D2D2D",
+        "primary-900": "#212121",
+        "secondary-400": "#F9F39B",
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolveText: "url('./assets/gym/EvolveText.png')"
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+}
+export default config
