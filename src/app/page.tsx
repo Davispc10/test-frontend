10135e91a8fb27bb36155596e9c77e60dfa137e0
João Feitoza <joaofeitoza.13@gmail.com>
2023-10-12 22:23:29 -0300

    style: fix responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import HomePage from '@/components/pages/HomePage'
 
 export const Home = () => {
-	return <HomePage styles="pt-3 h-full flex justify-center" />
+	return <HomePage styles="my-3 h-full flex justify-center" />
 }
 
 export default Home
