68ed81f3743bd2964edf7f6c9a26dfc535074c4e
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-05 18:03:03 -0300

    feat: layout and footer components added

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Suspense } from 'react'
+
+import { Container } from '@/components/container'
+import { Footer } from '@/components/footer'
+import { Search } from '@/components/search'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dinheirow',
+  description: 'Template for Dinheirow',
+}
+
+type Props = Readonly<{
+  children: React.ReactNode
+}>
+
+const RootLayout = ({ children }: Props) => {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <ScrollArea className="h-full w-full">
+          <Suspense>
+            <Search />
+          </Suspense>
+          <main>
+            <Container>{children}</Container>
+          </main>
+          <Footer />
+        </ScrollArea>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
