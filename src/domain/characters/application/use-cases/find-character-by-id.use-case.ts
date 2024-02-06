dadd1c18c4313db72e8372fa9817d94bcaddf69b
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-06 20:42:49 -0300

    feat: character use cases

--- src/domain/characters/application/use-cases/find-character-by-id.use-case.ts
+++ src/domain/characters/application/use-cases/find-character-by-id.use-case.ts
@@ -2,13 +2,15 @@ import { CharactersRepository } from '../repository/characters-repository'
 
 type Input = {
   id: string
+  hash: string
+  ts: number
 }
 
 export class FindCharacterByIdUseCase {
   constructor(private readonly charactersRepository: CharactersRepository) {}
 
-  async execute({ id }: Input) {
-    const character = await this.charactersRepository.findById({ id })
+  async execute({ id, hash, ts }: Input) {
+    const character = await this.charactersRepository.findById({ id, hash, ts })
 
     return character
   }
