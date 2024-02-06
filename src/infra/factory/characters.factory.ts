c91de762a27dd65fcc7b83d08ed80f6aeabfe709
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-06 18:10:14 -0300

    feat: characters factory

---
+++ src/infra/factory/characters.factory.ts
@@ -0,0 +1,19 @@
+import { FindAllCharactersUseCase } from '@/domain/characters/application/use-cases/find-all-characters.use-case'
+import { FindCharacterByIdUseCase } from '@/domain/characters/application/use-cases/find-character-by-id.use-case'
+
+import { CharactersController } from '../controller/characters.controller'
+import { CharactersGateway } from '../gateway/characters.gateway'
+import { HttpService } from '../http/http.service'
+
+export const charactersFactory = () => {
+  const http = new HttpService()
+
+  const gateway = new CharactersGateway(http)
+
+  const findAllUseCase = new FindAllCharactersUseCase(gateway)
+  const findById = new FindCharacterByIdUseCase(gateway)
+
+  const controller = new CharactersController(findAllUseCase, findById)
+
+  return controller
+}
