84939ea1ec0b240bb4bc143bab724ffbaa46726a
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-06 20:45:30 -0300

    feat: characters gateway

--- src/infra/gateway/characters.gateway.ts
+++ src/infra/gateway/characters.gateway.ts
@@ -1,12 +1,13 @@
-import { CharactersProps } from '@/@types/characters'
-import { defaultDescription, defaultImage } from '@/consts'
+import { CharacterProps, CharactersProps } from '@/@types/characters'
 import {
   CharactersRepository,
   FindAllCharactersProps,
   FindCharacterByIdProps,
 } from '@/domain/characters/application/repository/characters-repository'
+import { CharacterDetailsEntity } from '@/domain/characters/enterprise/characters-details.entity'
 import { CharacterEntity } from '@/domain/characters/enterprise/characters.entity'
 import { env } from '@/env.mjs'
+import { validateCharacter } from '@/utils/validate-character'
 import { validateCharacters } from '@/utils/validate-characters'
 
 import { HttpRepository } from '../http/http.repository'
@@ -26,10 +27,7 @@ export class CharactersGateway implements CharactersRepository {
 
       return characters
     }
-    console.log(page)
-    // console.log(
-    //   `${env.NEXT_PUBLIC_API_URL}/characters?ts=${ts}&limit=${limit}&offset=${page}&apikey=${env.NEXT_PUBLIC_API_PUBLIC_KEY}&hash=${hash}`,
-    // )
+
     const characters = await this.httpRepository.get<CharactersProps>({
       url: `${env.NEXT_PUBLIC_API_URL}/characters?ts=${ts}&limit=${limit}&offset=${page}&apikey=${env.NEXT_PUBLIC_API_PUBLIC_KEY}&hash=${hash}`,
     })
@@ -41,10 +39,14 @@ export class CharactersGateway implements CharactersRepository {
     return characters
   }
 
-  async findById({ id }: FindCharacterByIdProps): Promise<CharacterEntity> {
-    const character = await this.httpRepository.get<CharacterEntity>({
-      url: `${env.NEXT_PUBLIC_API_URL}/characters/${id}`,
+  async findById({ id, ts, hash }: FindCharacterByIdProps): Promise<CharacterDetailsEntity> {
+    const character = await this.httpRepository.get<CharacterDetailsEntity>({
+      url: `${env.NEXT_PUBLIC_API_URL}/characters/${id}?ts=${ts}&apikey=${env.NEXT_PUBLIC_API_PUBLIC_KEY}&hash=${hash}`,
     })
+
+    const charactersMapped = validateCharacter(character)
+
+    character.data.results = charactersMapped
 
     return character
   }
