013054bfefc845c543b3724d034e5ad241f5db03
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-06 20:41:48 -0300

    feat: generate static params

--- src/app/characters/[characterId]/page.tsx
+++ src/app/characters/[characterId]/page.tsx
@@ -2,25 +2,14 @@ import { Suspense } from 'react'
 
 import { Container } from '@/components/container'
 
-import { CharactersProps } from '@/@types/characters'
-import { QueryParams } from '@/lib/utils'
+import { CharacterEntity } from '@/domain/characters/enterprise/characters.entity'
 import { execute } from '@/utils/execute'
 
+import { Carousel, CarrouselSkeleton } from './carousel'
 import { Character, CharacterSkeleton } from './character'
 
 export async function generateStaticParams() {
-  const url = QueryParams.baseUrl('/characters')
-    .query({
-      query: 'limit',
-      value: '50',
-    })
-    .query({
-      query: 'offset',
-      value: '1',
-    })
-    .value()
-
-  const characters = await execute<CharactersProps>(url)
+  const characters = await execute<CharacterEntity>({ page: '1' })
 
   return characters.data.results.map((character) => ({
     characterId: String(character.id),
@@ -33,12 +22,22 @@ type Props = {
   }
 }
 
-const CharacterPage = ({ params: { characterId } }: Props) => {
+const CharacterPage = ({ params }: Props) => {
   return (
     <Container>
-      <Suspense fallback={<CharacterSkeleton />}>
-        <Character character={characterId} />
-      </Suspense>
+      <div className="space-y-10">
+        <Suspense fallback={<CharacterSkeleton />}>
+          <Character {...params} />
+        </Suspense>
+        <div className="space-y-3">
+          <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Quadrinhos em que aparece
+          </h2>
+          <Suspense fallback={<CarrouselSkeleton />}>
+            <Carousel {...params} />
+          </Suspense>
+        </div>
+      </div>
     </Container>
   )
 }
