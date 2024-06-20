4068152b8279b88e0aa875e47e634b76005f0d10
Pablo Alves <ribeirojr1522@gmail.com>
2024-06-20 11:51:34 -0300

    styles: :sparkles: fixed higth hero

--- marvel/src/app/modules/heroHome/index.tsx
+++ marvel/src/app/modules/heroHome/index.tsx
@@ -2,7 +2,7 @@ import { FilterNamePersonage, TitleHero } from './modules'
 
 export function HeroHome() {
   return (
-    <div className="bg-banner-personages bg-cover bg-center h-full max-h-[400px] ">
+    <div className="bg-banner-personages bg-cover bg-center h-[300px] sm:h-[400px]">
       <div className=" bg-black/30 w-full h-full flex justify-center items-center flex-col p-2">
         <TitleHero />
         <FilterNamePersonage />
