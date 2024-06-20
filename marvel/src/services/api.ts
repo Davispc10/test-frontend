ab8bb6e226bd9dfdfd3bfac3e345aa91daff8c64
Pablo Alves <ribeirojr1522@gmail.com>
2024-06-20 17:43:46 -0300

    feat: :sparkles: add params in api

--- marvel/src/services/api.ts
+++ marvel/src/services/api.ts
@@ -2,12 +2,14 @@ import { generateHashMarvel } from '@/utils'
 
 const time = new Date().getTime().toString()
 
-export function api(path: string, init?: RequestInit) {
+export function api(path: string, init?: RequestInit, params?: string) {
   const privateKey = process.env.NEXT_PUBLIC_PRIVATE_API_KEY
   const publicKey = process.env.NEXT_PUBLIC_API_KEY
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-  const routeUrl = `${baseUrl}/${path}?ts=${time}&apikey=${publicKey}&hash=${generateHashMarvel(
+  const paramsApi = params ? `${params}&` : ''
+
+  const routeUrl = `${baseUrl}/${path}?${paramsApi}ts=${time}&apikey=${publicKey}&hash=${generateHashMarvel(
     {
       time,
       privateKey,
