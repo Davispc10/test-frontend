4edbe5774c518cba2b7532fac8670493937c1de0
Pablo Alves <ribeirojr1522@gmail.com>
2024-06-20 21:13:29 -0300

    feat: :sparkles: validate not found personage

--- marvel/src/app/page.tsx
+++ marvel/src/app/page.tsx
@@ -14,6 +14,11 @@ export default function Home() {
       {!isLoading && data && (
         <Container className="py-4">
           <PaginationPersonages pages={pages} />
+          {!isLoading && data.length === 0 && (
+            <div className="flex items-center justify-center">
+              <p>characters not found</p>
+            </div>
+          )}
           <ListPersonages data={data} />
           <PaginationPersonages pages={pages} />
         </Container>
