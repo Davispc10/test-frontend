b38f88fc3d5bd7f77a8a010ed0688ed863d34cea
lhenriquedev <lhenrique.dev@gmail.com>
2024-07-05 21:36:00 -0300

    chore: change metadata and add more padding on app

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <div className="flex flex-1 flex-col p-8">{children}</div>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex flex-1 flex-col py-16 px-8">{children}</div>
     </div>
   )
 }
