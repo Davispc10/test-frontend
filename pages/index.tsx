ada93276cd692411e1bc9245bacc82b3cc825b55
josuerx12 <josueaze12@gmail.com>
2024-07-08 18:23:16 -0300

    core: new project started, with boilerplate next code.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>Teste Dinheirow</title>
+      </Head>
+
+      <h1>Olá</h1>
+    </main>
+  );
+};
+
+export default Home;
