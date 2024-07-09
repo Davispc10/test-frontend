6209141717b998a39690a4a6c21b075420b7826b
josuerx12 <josueaze12@gmail.com>
2024-07-09 19:46:12 -0300

    feat: new page to character details and comics.

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-import Loading from "@/components/molecules/Loading";
+
+import { descriptionValidation } from "@/libs/DescriptionValidation";
 import { fetchCharacterById } from "@/libs/fetchCharacterById";
+import { imagePathValidation } from "@/libs/ImagePathValidation";
 import { Undo2 } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { useQuery } from "react-query";
@@ -10,32 +13,44 @@ const CharacterDetails = ({ params }: { params: { id: string } }) => {
   const { isLoading, data } = useQuery(["character" + params.id], () =>
     fetchCharacterById(params.id)
   );
+  const { data: comicsData, isLoading: IsLoadingComics } = useQuery([
+    "characterComic" + params.id,
+  ]);
 
   const router = useRouter();
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <div className="bg-neutral-800   text-neutral-50 min-h-screen px-20 py-10">
-      <div className="max-w-screen-xl w-full mx-auto flex-col">
+      <div className="max-w-screen-xl gap-5 flex w-full mx-auto flex-col">
         <button
           onClick={router.back}
-          className="flex items-center gap-2 bg-slate-600 text-neutral-50 p-2 rounded font-semibold"
+          className="flex w-fit items-center gap-2 bg-slate-600 text-neutral-50 p-2 rounded font-semibold"
         >
-          Go Back <Undo2 />
+          Voltar <Undo2 />
         </button>
+        <div>
+          {data?.results.map((char) => {
+            return (
+              <div key={char.id} className="flex  justify-between">
+                <div>
+                  <h2 className="text-3xl font-semibold">{char.name}</h2>
+                  <p>{descriptionValidation(char.description)}</p>
+                </div>
 
-        {data?.results.map((char) => {
-          return (
-            <div key={char.id}>
-              <h3>{char.name}</h3>
-
-              <p>{char.description}</p>
-            </div>
-          );
-        })}
+                <Image
+                  src={imagePathValidation(
+                    char.thumbnail?.path,
+                    char.thumbnail?.extension
+                  )}
+                  width={400}
+                  height={400}
+                  className="rounded-md hover:shadow-lg hover:shadow-slate-50/25 hover:scale-105 duration-300 "
+                  alt={`Foto do personagem: ${char.name}`}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
