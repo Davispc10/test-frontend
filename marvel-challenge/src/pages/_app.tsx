8b74493fd9533d104beacdff3ae9829572a2b4cd
Rafael Panisset <rafaelpani99@gmail.com>
2024-07-13 16:59:14 -0300

    Feature: Adicionado componet MyAPP

---
+++ marvel-challenge/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default MyApp
