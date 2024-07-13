16b793e726a402749a2cfe9c170f553178cf5227
Carlos Edilson Junior <carlosedilsonabj@gmail.com>
2024-07-12 23:47:30 -0300

    config: add prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       Marvel App
+      <div className="p-2 pt-4"></div>
     </main>
-  );
+  )
 }
