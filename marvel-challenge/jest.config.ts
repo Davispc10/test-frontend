4c01b56ec525c7ed1fd73325ccd0395a2302180d
Rafael Panisset <rafaelpani99@gmail.com>
2024-07-13 14:26:57 -0300

    Feature: Adicionar arquivos de configuração, para inicialização do projeto, test, etc..

---
+++ marvel-challenge/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+};
+
+export default createJestConfig(config);
